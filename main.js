9932c34791ef560aca8a5e023b5e49971f3b287c
parkbomi-77 <vnstn785@naver.com>
2021-05-20 13:47:52 +0900

    미들웨어 body-parser

--- main.js
+++ main.js
@@ -5,6 +5,10 @@ var template = require('./lib/template.js');
 var path = require('path');
 var sanitizeHtml = require('sanitize-html');
 var qs = require('querystring');
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}));
 
 app.get('/', function (request, response) {
   fs.readdir('./data', function (error, filelist) {
@@ -63,19 +67,13 @@ app.get('/create', function (request, response) {
 });
 
 app.post('/create_process', function (request, response) {
-    var body = '';
-    request.on('data', function(data){
-        body = body + data;
-    });
-    request.on('end', function(){
-        var post = qs.parse(body);
-        var title = post.title;
-        var description = post.description;
-        fs.writeFile(`data/${title}`, description, 'utf8', function(err){
-          response.writeHead(302, {Location: `/?id=${title}`});
-          response.end();
-        })
-    });
+    var post = request.body;
+    var title = post.title;
+    var description = post.description;
+    fs.writeFile(`data/${title}`, description, 'utf8', function(err){
+      response.writeHead(302, {Location: `/?id=${title}`});
+      response.end();
+    })
 });
 
 app.get('/update/:pageId', function (request, response) {
@@ -105,58 +103,24 @@ app.get('/update/:pageId', function (request, response) {
 })
 
 app.post('/update_process', function (request, response) {
-    var body = '';
-    request.on('data', function(data){
-        body = body + data;
-    });
-    request.on('end', function(){
-        var post = qs.parse(body);
-        var id = post.id;
-        var title = post.title;
-        var description = post.description;
-        fs.rename(`data/${id}`, `data/${title}`, function(error){
-          fs.writeFile(`data/${title}`, description, 'utf8', function(err){
-            response.redirect(`?id=${title}`);
-          })
-        });
-    });
-})
+  var post = request.body;
+  var id = post.id;
+  var title = post.title;
+  var description = post.description;
+  fs.rename(`data/${id}`, `data/${title}`, function (error) {
+    fs.writeFile(`data/${title}`, description, 'utf8', function (err) {
+      response.redirect(`/?id=${title}`);
+    })
+  });
+});
 
 app.post('/delete_process', function (request, response) {
-    var body = '';
-    request.on('data', function(data){
-        body = body + data;
-    });
-    request.on('end', function(){
-        var post = qs.parse(body);
-        var id = post.id;
-        var filteredId = path.parse(id).base;
-        fs.unlink(`data/${filteredId}`, function(error){
-          response.redirect('/');
-        })
-    });
-})
+  var post = request.body;
+  var id = post.id;
+  var filteredId = path.parse(id).base;
+  fs.unlink(`data/${filteredId}`, function (error) {
+    response.redirect('/');
+  })
+});
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
-
-
-// var http = require('http');
-// var url = require('url');
-// 
-
-// var app = http.createServer(function(request,response){
-//     var _url = request.url;
-//     var queryData = url.parse(_url, true).query;
-//     var pathname = url.parse(_url, true).pathname;
-//     if(pathname === '/'){
-//       
-//     } else if(pathname === '/update_process'){
-//      
-//     } else if(pathname === '/delete_process'){
-//       
-//     } else {
-//       response.writeHead(404);
-//       response.end('Not found');
-//     }
-// });
-// app.listen(3000);
